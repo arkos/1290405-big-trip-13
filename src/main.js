34b34cdfa48cfdc0e2ad1abc86e09d3924671535
Kostya Kaganov <kkaganov@gmail.com>
2020-11-19 15:18:29 +0200

    Fix duplicate import of createEditEventTemplate

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import {createFiltersTemplate} from './view/filters.js';
 import {createSortTemplate} from './view/sort.js';
 import {createTripEventsTemplate} from './view/trip-events.js';
 import {createTripEventTemplate} from './view/trip-event.js';
-import {createEditEventTemplate, createEventEditTemplate} from './view/edit-event.js';
+import {createEditEventTemplate} from './view/edit-event.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
