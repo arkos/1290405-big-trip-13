04075c7385ca866ef08abfc1ff10cbe412fc8f9e
Kostya Kaganov <kkaganov@gmail.com>
2020-11-19 14:08:44 +0200

    Render menu and filters on the main page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import {createTripInfoTemplate} from './view/trip-info.js';
 import {createTripPriceTemplate} from './view/trip-price.js';
+import {createMenuTemplate} from './view/menu.js';
+import {createFiltersTemplate} from './view/filters.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -10,4 +12,10 @@ render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
 
 const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
 render(tripInfoElement, createTripPriceTemplate(), `beforeend`);
+
+const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
+const tripMenuTitleElement = tripControlsElement.querySelector(`h2`);
+
+render(tripMenuTitleElement, createMenuTemplate(), `afterend`);
+render(tripControlsElement, createFiltersTemplate(), `beforeend`);
 
