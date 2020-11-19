276fafaf9c0b3033aad2200af3c6f89d7f0252e7
Kostya Kaganov <kkaganov@gmail.com>
2020-11-19 13:19:04 +0200

    Render trip info and trip price on main page

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import {createTripInfoTemplate} from './view/trip-info.js';
+import {createTripPriceTemplate} from './view/trip-price.js';
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const tripMainElement = document.querySelector(`.trip-main`);
+render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
+
+const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
+render(tripInfoElement, createTripPriceTemplate(), `beforeend`);
+
