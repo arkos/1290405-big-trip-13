e559c4299c9bca882ed87368f52a2684be729abe
Kostya Kaganov <kkaganov@gmail.com>
2020-11-19 15:14:55 +0200

    Render edit event form inside the event list on the main page

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {createFiltersTemplate} from './view/filters.js';
 import {createSortTemplate} from './view/sort.js';
 import {createTripEventsTemplate} from './view/trip-events.js';
 import {createTripEventTemplate} from './view/trip-event.js';
+import {createEditEventTemplate, createEventEditTemplate} from './view/edit-event.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -27,6 +28,8 @@ render(tripEventsElement, createSortTemplate(), `beforeend`);
 
 render(tripEventsElement, createTripEventsTemplate(), `beforeend`);
 const tripEventsListElement = tripEventsElement.querySelector(`.trip-events__list`);
+
+render(tripEventsListElement, createEditEventTemplate(), `beforeend`);
 
 render(tripEventsListElement, createTripEventTemplate(), `beforeend`);
 render(tripEventsListElement, createTripEventTemplate(), `beforeend`);
