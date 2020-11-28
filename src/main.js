d7015427c00f0c814cdcd41d2987a031fd0152c0
Kostya Kaganov <kkaganov@gmail.com>
2020-11-28 02:02:20 +0200

    Render total price including offers

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import {generateEvent} from './mock/event.js';
 import {generateFilter} from './mock/filter.js';
 import {generateSort} from './mock/sort.js';
 
-const EVENT_COUNT = 4;
+const EVENT_COUNT = 20;
 
 const events = new Array(EVENT_COUNT).fill().map(generateEvent);
 
@@ -37,8 +37,12 @@ render(tripMainElement, createTripInfoTemplate(tripInfo), `afterbegin`);
 
 const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
 
-const totalPrice = sortedByDateEvents.reduce((sum, current) => current.price + sum, 0);
-render(tripInfoElement, createTripPriceTemplate(totalPrice), `beforeend`);
+const totalPriceForEvents = sortedByDateEvents.reduce((total, event) => {
+  const priceForEventOffers = event.offers.reduce((sum, offer) => sum + offer.price, 0);
+  return event.price + priceForEventOffers + total;
+}, 0);
+
+render(tripInfoElement, createTripPriceTemplate(totalPriceForEvents), `beforeend`);
 
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
 const tripMenuTitleElement = tripControlsElement.querySelector(`h2`);
