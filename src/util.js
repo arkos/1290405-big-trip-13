9ef84a5fad7559892e16fde923d585a5e1613f55
Kostya Kaganov <kkaganov@gmail.com>
2020-11-28 14:59:57 +0200

    Create corresponding utils for element and template rendering

--- src/util.js
+++ src/util.js
@@ -27,3 +27,31 @@ const shuffleItems = (items) => {
   }
   return shuffledItems;
 };
+
+export const RenderPosition = {
+  BEFOREBEGIN: `beforebegin`,
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  AFTEREND: `afterend`
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
