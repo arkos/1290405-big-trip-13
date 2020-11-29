01d4374a5074b6d4c9d0374dd7cb8e90364db6c3
Kostya Kaganov <kkaganov@gmail.com>
2020-11-29 17:56:52 +0200

    Improve description generation function

--- src/mock/event.js
+++ src/mock/event.js
@@ -1,11 +1,5 @@
 import dayjs from 'dayjs';
-
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
+import {getRandomInteger, getRandomItems} from '../util.js';
 
 const generateType = () => {
   const types = [
@@ -69,31 +63,11 @@ const generateDate = (offsetFromNow = 0, offsetUnit = `h`) => {
 };
 
 const generateDescription = () => {
-  const allDescriptions = [
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    `Cras aliquet varius magna, non porta ligula feugiat eget.`,
-    `Fusce tristique felis at fermentum pharetra.`,
-    `Aliquam id orci ut lectus varius viverra.`,
-    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-    `Sed sed nisi sed augue convallis suscipit in sed felis.`,
-    `Aliquam erat volutpat.`,
-    `Nunc fermentum tortor ac porta dapibus.`,
-    `In rutrum ac purus sit amet tempus.`
-  ];
+  const sampleDescription = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
+  const descriptions = Array.from(sampleDescription.matchAll(/([^.]*)\.\s*/g), (m) => m[1]);
 
-  const randomSize = getRandomInteger(1, allDescriptions.length);
-
-  const clonedDescriptions = [...allDescriptions];
-
-  const selectedDescriptions = [];
-
-  for (let i = 0; i < randomSize; i++) {
-    const randomIndex = getRandomInteger(0, clonedDescriptions.length - 1);
-    selectedDescriptions.push(clonedDescriptions.splice(randomIndex, 1));
-  }
-  return selectedDescriptions.join(` `);
+  const randomDescriptions = getRandomItems(descriptions);
+  return `${randomDescriptions.join(`. `)}.`;
 };
 
 const generatePrice = () => {
