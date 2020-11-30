9c837797fac75f7819f29bb1addae362d511dcf9
Kostya Kaganov <kkaganov@gmail.com>
2020-11-30 15:33:21 +0200

    Render no event using NoEventView class

--- src/main.js
+++ src/main.js
@@ -6,104 +6,111 @@ import SortView from './view/sort.js';
 import TripEventListView from './view/trip-event-list.js';
 import TripEventView from './view/trip-event.js';
 import EditEventView from './view/edit-event.js';
+import NoEventView from './view/no-event.js';
 import {generateEvent} from './mock/event.js';
 import {generateFilter} from './mock/filter.js';
 import {generateSort} from './mock/sort.js';
 import {render, RenderPosition, isEscEvent} from './util.js';
 
 const EVENT_COUNT = 20;
-
 const events = new Array(EVENT_COUNT).fill().map(generateEvent);
 
-const tripMainElement = document.querySelector(`.trip-main`);
-
-const sortedByDateEvents = [...events];
-sortedByDateEvents.sort((a, b) => a.startDate - b.startDate);
-
-const destinations = [];
-
-sortedByDateEvents.forEach((evt) => destinations.push(evt.destination));
-
-const tripInfo = {
-  startDate: sortedByDateEvents[0].startDate,
-  finishDate: sortedByDateEvents[sortedByDateEvents.length - 1].finishDate,
-  destinations
-};
-
-render(tripMainElement, new TripInfoView(tripInfo).getElement(), RenderPosition.AFTERBEGIN);
-
-const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
-
-const totalPriceForEvents = sortedByDateEvents.reduce((total, event) => {
-  const priceForEventOffers = event.offers.reduce((sum, offer) => sum + offer.price, 0);
-  return event.price + priceForEventOffers + total;
-}, 0);
-
-render(tripInfoElement, new TripPriceView(totalPriceForEvents).getElement(), RenderPosition.BEFOREEND);
-
-const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
-const tripMenuTitleElement = tripControlsElement.querySelector(`h2`);
-
-render(tripMenuTitleElement, new MenuView().getElement(), RenderPosition.AFTEREND);
-
-const filter = generateFilter();
-render(tripControlsElement, new FilterView(filter).getElement(), RenderPosition.BEFOREEND);
-
 const tripEventsElement = document.querySelector(`.trip-events`);
-const sort = generateSort();
-render(tripEventsElement, new SortView(sort).getElement(), RenderPosition.BEFOREEND);
 
-render(tripEventsElement, new TripEventListView().getElement(), RenderPosition.BEFOREEND);
+if (events.length === 0) {
+  render(tripEventsElement, new NoEventView().getElement(), RenderPosition.AFTERBEGIN);
+} else {
+  const sortedByDateEvents = [...events];
+  sortedByDateEvents.sort((a, b) => a.startDate - b.startDate);
 
-const tripEventsListElement = tripEventsElement.querySelector(`.trip-events__list`);
+  const destinations = [];
+  sortedByDateEvents.forEach((evt) => destinations.push(evt.destination));
 
-const renderTripEvent = (tripEventListElement, tripEvent) => {
-  const tripEventComponent = new TripEventView(tripEvent);
-  const tripEventEditComponent = new EditEventView(tripEvent);
+  const tripMainElement = document.querySelector(`.trip-main`);
 
-  const switchToEdit = () => {
-    tripEventListElement.replaceChild(tripEventEditComponent.getElement(), tripEventComponent.getElement());
+  const tripInfo = {
+    startDate: sortedByDateEvents[0].startDate,
+    finishDate: sortedByDateEvents[sortedByDateEvents.length - 1].finishDate,
+    destinations
   };
 
-  const switchToDisplay = () => {
-    tripEventListElement.replaceChild(tripEventComponent.getElement(), tripEventEditComponent.getElement());
-  };
+  render(tripMainElement, new TripInfoView(tripInfo).getElement(), RenderPosition.AFTERBEGIN);
 
-  const onClickRollupButtonUp = () => {
-    switchToDisplay();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  };
+  const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
 
-  const onClickRollupButtonDown = () => {
-    switchToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  };
+  const totalPriceForEvents = sortedByDateEvents.reduce((total, event) => {
+    const priceForEventOffers = event.offers.reduce((sum, offer) => sum + offer.price, 0);
+    return event.price + priceForEventOffers + total;
+  }, 0);
 
-  const onEscKeyDown = (evt) => {
-    isEscEvent(evt, () => {
+  render(tripInfoElement, new TripPriceView(totalPriceForEvents).getElement(), RenderPosition.BEFOREEND);
+
+  const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
+  const tripMenuTitleElement = tripControlsElement.querySelector(`h2`);
+
+  render(tripMenuTitleElement, new MenuView().getElement(), RenderPosition.AFTEREND);
+
+  const filter = generateFilter();
+  render(tripControlsElement, new FilterView(filter).getElement(), RenderPosition.BEFOREEND);
+
+  // const tripEventsElement = document.querySelector(`.trip-events`);
+  const sort = generateSort();
+  render(tripEventsElement, new SortView(sort).getElement(), RenderPosition.BEFOREEND);
+
+  render(tripEventsElement, new TripEventListView().getElement(), RenderPosition.BEFOREEND);
+
+  const tripEventsListElement = tripEventsElement.querySelector(`.trip-events__list`);
+
+  const renderTripEvent = (tripEventListElement, tripEvent) => {
+    const tripEventComponent = new TripEventView(tripEvent);
+    const tripEventEditComponent = new EditEventView(tripEvent);
+
+    const switchToEdit = () => {
+      tripEventListElement.replaceChild(tripEventEditComponent.getElement(), tripEventComponent.getElement());
+    };
+
+    const switchToDisplay = () => {
+      tripEventListElement.replaceChild(tripEventComponent.getElement(), tripEventEditComponent.getElement());
+    };
+
+    const onClickRollupButtonUp = () => {
       switchToDisplay();
       document.removeEventListener(`keydown`, onEscKeyDown);
-    });
+    };
+
+    const onClickRollupButtonDown = () => {
+      switchToEdit();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    };
+
+    const onEscKeyDown = (evt) => {
+      isEscEvent(evt, () => {
+        switchToDisplay();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      });
+    };
+
+    tripEventComponent.getElement()
+      .querySelector(`.event__rollup-btn`)
+      .addEventListener(`click`, onClickRollupButtonDown);
+
+    tripEventEditComponent.getElement()
+      .querySelector(`.event__rollup-btn`)
+      .addEventListener(`click`, onClickRollupButtonUp);
+
+    tripEventEditComponent.getElement()
+      .querySelector(`form`)
+      .addEventListener(`submit`, (evt) => {
+        evt.preventDefault();
+        switchToDisplay();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      });
+
+    render(tripEventListElement, tripEventComponent.getElement(), RenderPosition.AFTERBEGIN);
   };
 
-  tripEventComponent.getElement()
-    .querySelector(`.event__rollup-btn`)
-    .addEventListener(`click`, onClickRollupButtonDown);
+  sortedByDateEvents.forEach((tripEvent) =>
+    renderTripEvent(tripEventsListElement, tripEvent));
+}
 
-  tripEventEditComponent.getElement()
-    .querySelector(`.event__rollup-btn`)
-    .addEventListener(`click`, onClickRollupButtonUp);
 
-  tripEventEditComponent.getElement()
-    .querySelector(`form`)
-    .addEventListener(`submit`, (evt) => {
-      evt.preventDefault();
-      switchToDisplay();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    });
-
-  render(tripEventListElement, tripEventComponent.getElement(), RenderPosition.AFTERBEGIN);
-};
-
-sortedByDateEvents.forEach((tripEvent) =>
-  renderTripEvent(tripEventsListElement, tripEvent));
---
+++ src/view/no-event.js
@@ -0,0 +1,27 @@
+import {createElement} from '../util.js';
+
+const createNoTaskTemplate = () => {
+  return `<p class="trip-events__msg">Click New Event to create your first point</p>`;
+};
+
+export default class NoEvent {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoTaskTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
