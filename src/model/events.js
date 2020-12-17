6aa41a148593eddbd4ecb8025adce293617e6b7b
Kostya Kaganov <kkaganov@gmail.com>
2020-12-17 16:34:58 +0200

    Add methods to events model to add/update/delete an event

--- src/model/events.js
+++ src/model/events.js
@@ -13,4 +13,44 @@ export default class Events extends Subject {
   getEvents() {
     return this._events;
   }
+
+  updateEvent(updateType, update) {
+    const index = this._events.findIndex((event) => event.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't update a non existing event`);
+    }
+
+    this._events = [
+      ...this._events.slice(0, index),
+      update,
+      ...this._events.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  addEvent(updateType, update) {
+    this._events = [
+      update,
+      ...this._events
+    ];
+
+    this._notify(updateType, updateType);
+  }
+
+  deleteEvent(updateType, update) {
+    const index = this._events.findIndex((event) => event.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't delete a non existing event`);
+    }
+
+    this._events = [
+      ...this._events.slice(0, index),
+      ...this.events.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  }
 }
