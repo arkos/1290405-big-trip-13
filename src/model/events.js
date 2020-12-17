25b98a244f6493c0e68ea5494cdaf4ec5c963082
Kostya Kaganov <kkaganov@gmail.com>
2020-12-17 14:34:50 +0200

    Add the Events model which derives from Subject

---
+++ src/model/events.js
@@ -0,0 +1,16 @@
+import Subject from '../utils/subject.js';
+
+export default class Events extends Subject {
+  constructor() {
+    super();
+    this._events = [];
+  }
+
+  setEvents(events) {
+    this._events = events.slice();
+  }
+
+  getEvents() {
+    return this._events;
+  }
+}
