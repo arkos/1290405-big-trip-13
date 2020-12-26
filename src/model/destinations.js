30ff5732fdd147d1b40cd8998f2cb4220d5cca34
Kostya Kaganov <kkaganov@gmail.com>
2020-12-27 00:56:41 +0200

    Add destinations model

---
+++ src/model/destinations.js
@@ -0,0 +1,25 @@
+import Subject from '../utils/subject.js';
+
+export default class Destinations extends Subject {
+  constructor() {
+    super();
+    this._destinations = new Map();
+  }
+
+  setDestinations(destinations) {
+    this._destinations = new Map(destinations);
+  }
+
+  getDestinations() {
+    return this._destinations;
+  }
+
+  static adaptToClient(destinations) {
+    const adaptedDestinations = new Map();
+
+    destinations.forEach((destination) =>
+      adaptedDestinations.set(destination.name, {
+        description: destination.description, photos: [...destination.pictures]
+      }));
+  }
+}
