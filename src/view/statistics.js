033cb3ee3b8c276b642c150fc706d869b5444c7d
Kostya Kaganov <kkaganov@gmail.com>
2021-01-08 11:29:31 +0200

    Create methods and functions to render charts

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -1,6 +1,149 @@
 import SmartView from '../view/smart.js';
+import Chart from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import {pointTypes} from '../utils/const.js';
 
-const createStatisticsTemplate = () => {
+const renderMoneyChart = (moneyCtx) => {
+  return new Chart(moneyCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`TAXI`, `BUS`, `TRAIN`, `SHIP`, `TRANSPORT`, `DRIVE`],
+      datasets: [{
+        data: [400, 300, 200, 160, 150, 100],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: `end`,
+          align: `start`,
+          formatter: (val) => `€ ${val}`
+        }
+      },
+      title: {
+        display: true,
+        text: `MONEY`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+
+const renderTypeChart = (typeCtx) => {
+  return new Chart(typeCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`TAXI`, `BUS`, `TRAIN`, `SHIP`, `TRANSPORT`, `DRIVE`],
+      datasets: [{
+        data: [4, 3, 2, 1, 1, 1],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: `end`,
+          align: `start`,
+          formatter: (val) => `${val}x`
+        }
+      },
+      title: {
+        display: true,
+        text: `TYPE`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+
+const renderTimeChart = () => {
+
+};
+
+const createStatisticsTemplate = (state) => {
+  const {points} = state;
+
   return `<section class="statistics statistics--hidden">
     <h2 class="visually-hidden">Trip statistics</h2>
 
@@ -28,6 +171,10 @@ export default class Statistics extends SmartView {
 
     this._dateChangeHandler = this._dateChangeHandler.bind(this);
 
+    this._moneyChart = null;
+    this._typeChart = null;
+    this._timeChart = null;
+
     this._setCharts();
   }
 
@@ -43,6 +190,30 @@ export default class Statistics extends SmartView {
   }
 
   _setCharts() {
+    if (this._moneyChart !== null || this._typeChart !== null || this._timeChart !== null) {
+      this._moneyChart = null;
+      this._typeChart = null;
+      this._timeChart = null;
+    }
 
+    const {points} = this._state;
+
+    const moneyCtx = document.querySelector(`.statistics__chart--money`);
+    const typeCtx = document.querySelector(`.statistics__chart--transport`);
+    const timeCtx = document.querySelector(`.statistics__chart--time`);
+
+    this._moneyChart = renderMoneyChart(moneyCtx, points);
+    this._typeChart = renderTypeChart(typeCtx, points);
+    this._timeChart = renderTimeChart(timeCtx, points);
+  }
+
+  removeElement() {
+    super.removeElement();
+
+    if (this._moneyChart !== null || this._typeChart !== null || this._timeChart !== null) {
+      this._moneyChart = null;
+      this._typeChart = null;
+      this._timeChart = null;
+    }
   }
 }
